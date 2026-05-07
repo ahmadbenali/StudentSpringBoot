a9300894bd1601921815b5b9af085d2c8a9c412b
“ahmadbenali” <a7madbenali@gmail.com>
2026-05-07 15:53:18 +0300

    add exception handler

--- src/main/java/com/example/SpringBoot/Controller/StudentController.java
+++ src/main/java/com/example/SpringBoot/Controller/StudentController.java
@@ -1,11 +1,12 @@
 package com.example.SpringBoot.Controller;
 
 import com.example.SpringBoot.Entity.Student;
+import com.example.SpringBoot.Exception.StudentErrorResponse;
+import com.example.SpringBoot.Exception.StudentNotFoundExecption;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +37,35 @@ public class StudentController {
 
     @GetMapping("/student/{studentId}")
     public Student getStudent(@PathVariable int studentId){
+
+        if(studentId < 0 || studentId > theStudents.size()){
+            throw new StudentNotFoundExecption("Invalid student id");
+        }
+
         return theStudents.get(studentId);
+    }
+
+    // this if you enter an int not found
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundExecption exc) {
+
+        StudentErrorResponse error=new StudentErrorResponse();
+        error.setErrorCode(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+
+    }
+    //this for all type of exception, like if you typed string instead of int
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        StudentErrorResponse error=new StudentErrorResponse();
+        error.setErrorCode(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 }
